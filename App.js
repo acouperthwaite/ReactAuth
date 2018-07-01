3d124b0aec14c872a1dcf7aad5f671fb7cadf6ab
Andrew Couperthwaite <acouperthwaite@gmail.com>
2018-07-01 15:52:45 -0600

    init commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { Component} from 'react';
+import { Text, View } from 'react-native';
+
+class App extends Component {
+  render() {
+    return(
+      <View>
+        <Text>An app</Text>
+      </View>
+    )
+  }
+}
+
+export default App;
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <Text>Changes you make will automatically reload.</Text>
+//         <Text>Shake your phone to open the developer menu.</Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
